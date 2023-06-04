5129ee16947652bdc56bee83c6e89bb417fd98d5
madong <viaimperiale@gmail.com>
2023-06-04 20:25:08 +0200

    build(be/Docker): using gunicorn for production mode

--- be/app.py
+++ be/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    if os.getenv('FLASK_ENV') != 'production':
+        app.run(host='0.0.0.0', port=3000)
