992dbdd098a5c05a72a26746a0e2180c55ada48e
madong <viaimperiale@gmail.com>
2023-06-21 22:21:14 +0200

    build: add HeroIcons and prettier

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -3,12 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [import('flowbite/plugin')],
-}
-
+  plugins: [import("flowbite/plugin")],
+};
