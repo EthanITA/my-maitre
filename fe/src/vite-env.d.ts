313739e8ede58adb2563a9cf63bf4e2281110795
madong <viaimperiale@gmail.com>
2023-06-22 20:54:24 +0200

    fix(i18n): missing types

--- fe/src/vite-env.d.ts
+++ fe/src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+import { VueI18n } from "vue-i18n";
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $t: VueI18n["t"];
+    $tc: VueI18n["tc"];
+    $te: VueI18n["te"];
+    $d: VueI18n["d"];
+    $n: VueI18n["n"];
+    $tm: VueI18n["tm"];
+    $locale: VueI18n["locale"];
+    $getLocales: VueI18n["getLocales"];
+    $setLocale: VueI18n["setLocale"];
+    $i18n: VueI18n;
+  }
+}
