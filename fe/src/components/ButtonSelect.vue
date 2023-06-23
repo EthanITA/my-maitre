20a0c8087a6d40738627d207697bcae13f035617
madong <viaimperiale@gmail.com>
2023-06-23 14:41:40 +0200

    fix(ButtonSelect): add vertical

--- fe/src/components/ButtonSelect.vue
+++ fe/src/components/ButtonSelect.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button, ButtonGroup } from "flowbite-vue";
+import { ButtonVariant } from "flowbite-vue/dist_types/components/Button/types";
 
 type Option = {
   name: string;
@@ -11,9 +12,14 @@ defineProps<{
   modelValue?: Option["value"];
   label?: string;
   disabled?: boolean;
+  vertical?: boolean;
+  color?: ButtonVariant;
 }>();
 </script>
-
+"text-blue-700 border-blue-700 dark:border-blue-500 dark:text-blue-500
+hover:text-white dark:hover:text-white dark:hover:bg-blue-600 text-blue-700
+border border-blue-700 focus text-sm text-center dark:border-blue-500
+dark:text-blue-500 hover:text-white dark:hover:text-white
 <template>
   <div>
     <label
@@ -21,14 +27,21 @@ defineProps<{
       class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
       >{{ label }}</label
     >
-    <ButtonGroup class="shadow-none">
+    <ButtonGroup class="shadow-none" :class="{ 'flex-col': vertical }">
       <Button
+        :color="color || 'default'"
         :disabled="disabled || false"
         :outline="
           JSON.stringify(option.value) !== JSON.stringify(modelValue || '')
         "
         v-for="option in options"
-        class="rounded-none focus:!ring-2 first:rounded-l last:rounded-r border-r-0 last:border-r-[1px]"
+        class="rounded-none focus:!ring-0"
+        :class="{
+          'first:rounded-l last:rounded-r border-r-0 last:border-r-[1px]':
+            !vertical,
+          'first:rounded-t last:rounded-b border-b-0 last:border-b-[1px]':
+            vertical,
+        }"
         @click="$emit('update:modelValue', option.value)"
       >
         {{ option.name }}
