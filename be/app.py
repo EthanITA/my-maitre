fe3c8aa51fde97ccb596fcbcbb6133867f737fdb
madong <viaimperiale@gmail.com>
2023-06-25 21:14:34 +0200

    build(BE): add Cors

--- be/app.py
+++ be/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
 import os
 
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
+@cross_origin()
 def hello_world():  # put application's code here
     return 'Hello World!'
 
