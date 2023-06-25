d0304f6bb0ec032894a482b7a61133358fc719aa
madong <viaimperiale@gmail.com>
2023-06-25 20:58:48 +0200

    build: add Pinia

--- fe/src/main.ts
+++ fe/src/main.ts
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import "./index.css";
 import router from "./router";
 import i18n from "./i18n";
+import { createPinia } from "pinia";
 
-createApp(App).use(router).use(i18n).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).use(i18n).mount("#app");
