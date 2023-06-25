63614bee4bb94d7235ac13607948e63dadd4c0c1
madong <viaimperiale@gmail.com>
2023-06-25 23:38:50 +0200

    build(BE): add dotenv and Flask-SQLAlchemy

--- be/routes/__init__.py
+++ be/routes/__init__.py
@@ -1,8 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 from be.routes.API import register_api_routes
 from be.routes.Menu import MenuView
 
 app = Flask(__name__)
+
+load_dotenv()
+assert os.getenv('DATABASE_URL') is not None
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 register_api_routes(app, '/menus', MenuView)
