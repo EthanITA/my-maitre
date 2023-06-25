e5b69447943ef26a2e3be3d40acca1b0ff6535b7
madong <viaimperiale@gmail.com>
2023-06-25 02:08:08 +0200

    fix(Sheet): add header

--- fe/src/components/Sheet.vue
+++ fe/src/components/Sheet.vue
@@ -3,6 +3,9 @@
 <template>
   <div class="bg-white rounded-lg shadow-sm w-full">
     <div class="m-4 overflow-x-auto" v-bind="$attrs">
+      <div class="my-2" v-if="$slots.header">
+        <slot name="header" />
+      </div>
       <slot />
     </div>
     <div class="p-4 bg-white/30 border-t-[1px] flex">
