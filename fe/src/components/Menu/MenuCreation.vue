0381725d5afb02c82d42d006531e009511ad7df0
madong <viaimperiale@gmail.com>
2023-07-01 13:25:09 +0200

    fix(MenuCreation): finish the implementation

--- fe/src/components/Menu/MenuCreation.vue
+++ fe/src/components/Menu/MenuCreation.vue
@@ -1,49 +1,3 @@
-<script setup lang="ts">
-import Container from "../Container.vue";
-import Sheet from "../Sheet.vue";
-import { Alert, Badge, Button, Input } from "flowbite-vue";
-import Menu, { MenuItem, menuTypes, visibilities } from "../../models/Menu";
-import Divider from "../Divider.vue";
-import { reactive, ref } from "vue";
-import ButtonSelect from "../ButtonSelect.vue";
-import Toggle from "../Toggle.vue";
-import DaySelect from "../DaySelect.vue";
-import { PlusIcon } from "@heroicons/vue/24/solid";
-import { Weekday } from "../../models/Custom/DatetimeTypes.ts";
-
-const form = reactive<MenuItem>({
-  id: 0,
-  name: "",
-  description: "",
-  icon: "",
-  hide_price: false,
-  order_type: "standard",
-  visibility: {},
-  location_id: 0,
-});
-
-const enableVisilibity = ref<boolean>(false);
-// yyyy-MM-dd
-const selectedDate = ref<string>(new Date().toISOString().split("T")[0]);
-const errorText = ref<string>("");
-
-const handleAddDate = () => {
-  if (selectedDate.value) {
-    (form!.visibility!.availability as string[]).push(selectedDate.value);
-  }
-};
-
-const handleSubmit = async () => {
-  errorText.value = "";
-  const isValid = Menu.validate(form);
-  if (!isValid) {
-    errorText.value = "menu.creation.error";
-    return;
-  }
-  await new Menu(form).create();
-};
-</script>
-
 <template>
   <Container
     title="menu.creation.label"
@@ -96,49 +50,69 @@ const handleSubmit = async () => {
                 value: visibility,
               }))
             "
-            :modelValue="form.visibility!.type"
-            @update:modelValue="($event)=>{
-            form.visibility!.type = $event
-            form.visibility!.availability = []
-          }"
+            :modelValue="form.visibility.type"
+            @update:modelValue="
+              ($event) => {
+                form.visibility.type = $event;
+                form.visibility.availability = [];
+              }
+            "
           />
           <template v-if="enableVisilibity">
-            <template v-if="form.visibility!.type === 'everyday'">
+            <template v-if="form.visibility.type === 'everyday'">
               <Input
                 type="time"
-                v-model="form!.visibility!.availability![0]"
+                v-model="form.visibility.availability![0]"
                 :label="$t('menu.fields.startTime')"
               />
               <Input
                 type="time"
                 :label="$t('menu.fields.endTime')"
-                v-model="form!.visibility!.availability![1]"
+                v-model="form.visibility.availability![1]"
               />
             </template>
-            <template v-else-if="form.visibility!.type === 'weekdays'">
+            <template v-else-if="form.visibility.type === 'weekdays'">
               <DaySelect
                 multiple
-                v-model="form!.visibility!.availability as Weekday[]"
+                v-model="form.visibility.availability as Weekday[]"
               />
             </template>
-            <template v-else-if="form.visibility!.type === 'days'">
-              <div class="flex flex-col">
+            <template v-else-if="form.visibility.type === 'days'">
+              <div class="flex flex-col gap-2">
                 <div class="flex gap-1">
                   <Input
                     type="date"
                     v-model="selectedDate"
                     :label="$t('menu.fields.days')"
                   />
-                  <Button pill square class="mt-auto" @click="handleAddDate">
+                  <Button
+                    pill
+                    square
+                    class="mt-auto"
+                    type="button"
+                    @click="handleAddDate"
+                  >
                     <PlusIcon class="w-5 h-5" />
                   </Button>
                 </div>
-                <div class="flex gap-1">
+                <div class="flex flex-wrap gap-1">
                   <Badge
-                    v-for="day in form!.visibility!.availability"
+                    v-for="day in form.visibility.availability"
                     :key="day"
+                    class="justify-between !text-xs pr-1 font-bold flex items-center gap-1 py-1 px-3"
                   >
-                    {{ day }}
+                    <p>{{ new Date(day).toLocaleDateString() }}</p>
+                    <div
+                      class="rounded-full p-0.5 cursor-pointer"
+                      @click="
+                        form.visibility.availability = _.filter(
+                          form.visibility.availability as string[],
+                          (d) => d !== day
+                        )
+                      "
+                    >
+                      <XMarkIcon class="w-3 h-3 text-red-700" />
+                    </div>
                   </Badge>
                 </div>
               </div>
@@ -154,5 +128,56 @@ const handleSubmit = async () => {
     </form>
   </Container>
 </template>
+<script setup lang="ts">
+import Container from "../Container.vue";
+import Sheet from "../Sheet.vue";
+import { Alert, Badge, Button, Input } from "flowbite-vue";
+import Menu, { MenuItem, menuTypes, visibilities } from "../../models/Menu";
+import Divider from "../Divider.vue";
+import { reactive, ref } from "vue";
+import ButtonSelect from "../ButtonSelect.vue";
+import Toggle from "../Toggle.vue";
+import DaySelect from "../DaySelect.vue";
+import { PlusIcon, XMarkIcon } from "@heroicons/vue/24/solid";
+import { Weekday } from "../../models/Custom/DatetimeTypes.ts";
+import _ from "lodash";
+
+const props = defineProps<{
+  form?: MenuItem;
+}>();
+
+const form = reactive<MenuItem>({
+  name: props.form?.name ?? "",
+  description: props.form?.description ?? "",
+  icon: props.form?.icon ?? "",
+  hide_price: props.form?.hide_price ?? false,
+  order_type: props.form?.order_type ?? "standard",
+  visibility: props.form?.visibility ?? {},
+  location_id: props.form?.location_id ?? 0,
+});
+
+const enableVisilibity = ref<boolean>(false);
+// yyyy-MM-dd
+const selectedDate = ref<string>(new Date().toISOString().split("T")[0]);
+const errorText = ref<string>("");
+
+const handleAddDate = () => {
+  let dates = form.visibility.availability as string[];
+  if (selectedDate.value) {
+    dates.push(selectedDate.value);
+    form.visibility.availability = _.sortBy(_.uniq(dates));
+  }
+};
+
+const handleSubmit = async () => {
+  errorText.value = "";
+  const isValid = Menu.validate(form);
+  if (!isValid) {
+    errorText.value = "menu.creation.error";
+    return;
+  }
+  await new Menu(form).create();
+};
+</script>
 
 <style scoped></style>
