c90a9edc58fd302808afbe583fb4cc756b6c7859
madong <viaimperiale@gmail.com>
2023-07-01 18:56:19 +0200

    fix(loader): use debounce of 150ms before trigger it

--- fe/src/store/loader.ts
+++ fe/src/store/loader.ts
@@ -1,11 +1,12 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import _ from "lodash";
 
 export default defineStore("loaderStore", () => {
   const loading = ref(false);
-  const setLoading = (value: boolean) => {
+  const setLoading = _.debounce((value: boolean) => {
     loading.value = value;
-  };
+  }, 150);
   return {
     loading,
     setLoading,
