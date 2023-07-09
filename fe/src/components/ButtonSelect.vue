b1a5c3c824840ce80f48b59145fb90b641b4cf1e
madong <viaimperiale@gmail.com>
2023-07-09 23:30:00 +0200

    fix(ButtonSelect): add multiple selection

--- fe/src/components/ButtonSelect.vue
+++ fe/src/components/ButtonSelect.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { Button, ButtonGroup } from "flowbite-vue";
 // @ts-ignore
 import { ButtonVariant } from "flowbite-vue/dist_types/components/Button/types";
@@ -8,14 +8,44 @@ type Option = {
   value: any;
 };
 
-defineProps<{
+const props = defineProps<{
   options: Option[];
-  modelValue?: Option["value"];
+  modelValue?: Option["value"] | Option["value"][];
   label?: string;
   disabled?: boolean;
   vertical?: boolean;
   color?: ButtonVariant;
+  multiple?: boolean;
 }>();
+
+const emit = defineEmits<{
+  "update:modelValue": (value: Option["value"] | Option["value"][]) => void;
+}>();
+
+const isSelected = (option: Option): boolean => {
+  if (props.multiple) {
+    return !!(props.modelValue as Option["value"][]).find(
+      (value) => JSON.stringify(value) === JSON.stringify(option.value)
+    );
+  } else {
+    return (
+      JSON.stringify(option.value) === JSON.stringify(props.modelValue || "")
+    );
+  }
+};
+
+const emitUpdate = (value: Option["value"] | Option["value"][]): void => {
+  if (props.multiple) {
+    emit("update:modelValue", [
+      ...(props.modelValue as Option["value"][]).filter(
+        (v) => JSON.stringify(v) !== JSON.stringify(value)
+      ),
+      value,
+    ]);
+  } else {
+    emit("update:modelValue", value);
+  }
+};
 </script>
 <template>
   <div>
@@ -24,23 +54,21 @@ defineProps<{
       class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
       >{{ label }}</label
     >
-    <ButtonGroup class="shadow-none" :class="{ 'flex-col': vertical }">
+    <ButtonGroup :class="{ 'flex-col': vertical }" class="shadow-none">
       <Button
-        type="button"
-        :color="color || 'default'"
-        :disabled="disabled || false"
-        :outline="
-          JSON.stringify(option.value) !== JSON.stringify(modelValue || '')
-        "
         v-for="option in options"
-        class="rounded-none focus:!ring-0"
         :class="{
           'first:rounded-l last:rounded-r border-r-0 last:border-r-[1px]':
             !vertical,
           'first:rounded-t last:rounded-b border-b-0 last:border-b-[1px]':
             vertical,
         }"
-        @click="$emit('update:modelValue', option.value)"
+        :color="color || 'default'"
+        :disabled="disabled || false"
+        :outline="!isSelected(option)"
+        class="rounded-none focus:!ring-0"
+        type="button"
+        @click="emitUpdate(option.value)"
       >
         {{ option.name }}
       </Button>
