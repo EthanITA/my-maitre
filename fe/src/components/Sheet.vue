808eaea1194883be8b66d55ce1315117ea9b32c7
madong <viaimperiale@gmail.com>
2023-07-16 20:36:17 +0200

    fix(Sheet): add disabled

--- fe/src/components/Sheet.vue
+++ fe/src/components/Sheet.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean;
+}>();
+</script>
 
 <template>
-  <div class="bg-white rounded-lg shadow-sm w-full">
+  <div
+    class="bg-white rounded-lg shadow-sm w-full"
+    :class="{
+      relative: disabled,
+    }"
+  >
     <div class="m-4 overflow-x-auto" v-bind="$attrs">
       <div class="my-2" v-if="$slots.header">
         <slot name="header" />
@@ -11,6 +20,11 @@
     <div v-if="$slots.footer" class="p-4 bg-white/30 border-t-[1px] flex">
       <slot name="footer" />
     </div>
+
+    <div
+      v-if="disabled"
+      class="absolute inset-0 rounded-lg bg-white/60 cursor-not-allowed"
+    />
   </div>
 </template>
 
