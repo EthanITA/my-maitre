ee9901aa47cd6d053a568a56fb601bbfdd65186a
madong <viaimperiale@gmail.com>
2023-07-27 21:36:54 +0200

    feat(Order): add table enc/dec routes

--- be/routes/Order.py
+++ be/routes/Order.py
@@ -1,6 +1,11 @@
+import json
+
+from flask import make_response, request, jsonify
+
 from .API import RouteMethodView
 from .CRUD import CRUD
 from .db import db
+from .utilities import Cipher
 
 
 class Order(db.Model):
@@ -17,8 +22,50 @@ class Order(db.Model):
     status = db.Column(db.Text)
 
 
+class DecryptedTable:
+    spot: str
+    menu_type: str
+
+
 class OrderView(CRUD, RouteMethodView):
     route = Order.__tablename__.lower()
 
     def __init__(self):
         super().__init__(Order)
+
+    def get_encrypted_table(self):
+        try:
+            spot = request.args.get('spot')
+            menu_type = request.args.get('menu_type')
+            return Cipher.encrypt(json.dumps({
+                'spot': spot,
+                'menu_type': menu_type
+            }))
+        except ValueError:
+            return make_response('Bad request', 400)
+        except Exception as e:
+            return make_response(str(e), 500)
+
+    def get_decrypted_table(self, encrypted_table: str):
+        try:
+            decrypted_table: DecryptedTable = json.loads(Cipher.decrypt(encrypted_table))
+            if 'spot' not in decrypted_table or 'menu_type' not in decrypted_table:
+                raise ValueError
+            return decrypted_table
+        except ValueError:
+            return make_response('Bad request', 400)
+        except Exception as e:
+            return make_response(str(e), 500)
+
+    @property
+    def custom_routes(self) -> dict[str, dict[str, any]]:
+        return {
+            'table': {
+                'methods': ['GET'],
+                'view_func': self.get_encrypted_table
+            },
+            'table/<str:encrypted_table>': {
+                'methods': ['GET'],
+                'view_func': self.get_decrypted_table
+            }
+        }
